705cd9234c4fc7c76bfa4fb660856dd092c72308
bharath <gowda00bharath@gmail.com>
2026-09-24 22:25:30 +0530

    Added all the Basic Introduction for python

---
+++ Introduction.py
@@ -0,0 +1,68 @@
+#Learning the Basic
+
+print("hello bharath","i am god")
+print(23)
+print(23+7)
+name="Bharath"
+age=21
+price=20.11
+print("my name is :",name)
+print("my age is :", age)
+output=True
+a=None
+
+# Knowing the Type of the data 
+
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(output))
+print(type(a))
+
+# learning Operator
+
+b=10
+c=29
+sum=b+c
+diff=b-c
+print(diff)
+print(sum)
+
+print(not True) # oprator useage
+print(not False) #Not operator useage
+
+val1=False
+val2=False
+
+print(val1 and val2) # and operator
+print(val1 or val2) # or operator
+
+# # Typecasting
+
+d=int("2")
+e=8.23
+summ=d+e
+print(summ)
+
+namee="Bharath "
+lastname="gowda"
+add=namee+lastname
+
+print(add)
+
+# input taken from te user 
+
+firstname=input("Enter your name")
+print("Welcome",firstname)
+
+value=int(input("Enter a val"))
+print(type(value),value)
+
+
+# Learning programing
+
+n1 = int(input("Enter the first value "))
+n2=int(input("Enter the second vlaue"))
+
+pow=n1**n2
+print(pow)
