730fba4bcb80bef44e4bf9d1d3e583e5753d1ac6
bharath <gowda00bharath@gmail.com>
2026-09-26 19:54:41 +0530

    lecture2 of python

---
+++ Leacture2.py
@@ -0,0 +1,80 @@
+# # Escape Sequence
+
+# \n is used for next line
+# \t is used for tab space
+
+# Concatination
+
+# str1+str2=new_str
+# "Bharath"+"gowda"="Bharathgowda"
+
+#length of the string
+# print(len(str1))
+# print(len(str2))
+# print(len(new_str))
+
+# Indexing -> We can only accses the data at the index but can't modify
+
+# str="Bharath"
+# ch=str[0]
+# print(ch)->"B"
+# print(str[5]) -> t
+
+# Slicing
+
+# str="apna college"
+# print(str[1:4])
+# print(str[:5])
+# print(str[3:])
+# print(str[1:len(str)])
+
+# String function
+
+# str="i  am learning python"
+
+# print(str.endswith("on"))
+
+# print(str.capitalize())
+
+# print(str.replace("python","java"))
+
+# print(str.find("m"))
+
+# print(str.count("am"))
+# print(str)
+
+#practice program1
+
+# str1=str(input("write first name"))
+
+# print(len(str1))
+
+#practice program2
+# str1=str(input("write first name"))
+
+# print(str1.count("$"))
+
+# if condition statement
+
+# age=18
+
+# if(age >18):
+#     print("eligible for voteing")
+# elif(age == 18):
+#     print("should make voter id first")
+# else:
+#     print("go and sleep")
+
+
+# print("end of code")
+
+#program 3
+
+n1=int(input("enter the number"))
+
+if(n1%7==0):
+    print("Entered number is multiple of 7")
+else:
+    print("it's not multiple of 7")
+
+#program4
